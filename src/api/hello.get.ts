6c43d41b48f2481b910491f454f3809204464ebd
Hardel <42413417+Hardel-DW@users.noreply.github.com>
2023-02-06 16:43:53 +0100

    Update hello.get.ts

--- src/api/hello.get.ts
+++ src/api/hello.get.ts
@@ -1,17 +1,6 @@
 import { Request, Response } from 'express';
 
-export type hey = {
-    a: string;
-}
-
 export default function handle(req: Request, res: Response) {
-    const a: hey = {
-        a: "Hello World"
-    }
-
-    console.log(a);
-
-
     res.status(200).json({
         name: "Hello World"
     });
